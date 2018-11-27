506fb6eb7dd7efd869fa4bdc207651bbf241d810
Peter Kurlak <pkurlak@gmail.com>
2018-11-27 00:35:22 -0600

    Fix config name and add colors variable

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,18 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char *fonts[]          = { "Source Code Pro:size=12" };
-static const char dmenufont[]       = "Source Code Pro:size=12";
-static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#8312ce";
-static const char selbgcolor[]      = "#222222";
-static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Source Code Pro:size=12" };
+static const char dmenufont[]       = "Source Code Pro:size=12";
+static const char normfgcolor[]     = "#bbbbbb";
+static const char normbgcolor[]     = "#222222";
+static const char normbordercolor[] = "#444444";
+static const char selfgcolor[]      = "#eeeeee";
+static const char selbgcolor[]      = "#222222";
+static const char selbordercolor[]  = "#8312ce";
+static const char *colors[][3]      = {
+        /*               fg         bg         border   */
+        [SchemeNorm] = { normalfgcolor, normalbgcolor, normalbordercolor },
+        [SchemeSel]  = { selfgcolor, selbgcolor, selbordercolor },
+};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
