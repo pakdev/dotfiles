62bcec2683f12afeef99acfb722d05e4106d2ac6
Peter Kurlak <pkurlak@gmail.com>
2018-11-27 09:31:50 -0600

    Add missing functions and prevent antibody from being downloaded multiple times

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-curl -sL https://git.io/antibody | sh -s
-antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
+if [ ! -f /usr/local/bin/antibody ]; then 
+	curl -sL https://git.io/antibody | sh -s
+fi
+antibody bundle < "$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
 antibody update
