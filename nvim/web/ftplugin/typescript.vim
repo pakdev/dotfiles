8387de3da3909496c44a942e95bb2047d28e7a34
Peter Kurlak <peter.kurlak@emerson.com>
2025-12-03 14:51:41 -0600

    Delete old files

--- nvim/web/ftplugin/typescript.vim
+++
@@ -1,6 +0,0 @@
-" Fix files with prettier followed by ESlint
-let b:ale_fixers = ['prettier', 'eslint']
-
-" Automatic imports
-let b:ale_completion_tsserver_autoimport = 1
-
--- old/antibody/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ ! -f /usr/local/bin/antibody ]; then 
-	curl -sL https://git.io/antibody | sh -s
-fi
-antibody bundle < "$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
-antibody update
