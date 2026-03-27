8818730aadcf99ba98fcfa21079caf88ed41dabb
Peter Kurlak <peter.kurlak@emerson.com>
2026-03-27 13:12:16 -0500

    Update nvim and add tmux

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Beautify JSON using jq
+vim.keymap.set({ "n", "v" }, "<leader>jM", function()
+  local mode = vim.fn.mode()
+  if mode == "n" then
+    vim.cmd("%!jq '.'")
+  else
+    vim.cmd("'<,'>!jq '.'")
+  end
+end, { desc = "Beautify JSON (jq)" })
+
 -- Override ZQ and ZZ to quit all windows (including NeoTree)
 vim.keymap.set("n", "ZQ", "<cmd>qa<cr>", { desc = "Quit all windows" })
 vim.keymap.set("n", "ZZ", "<cmd>wqa<cr>", { desc = "Save all and quit all windows" })
